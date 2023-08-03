4843695e06dece66c862f25d99b15eb992aeafc8
Ali George <george7ali@gmail.com>
2023-08-03 19:16:01 +0100

    created the start and function and implemented the start button to handle initiating the game

--- script.js
+++ script.js
@@ -31,7 +31,19 @@ const Game = (() => {
     let currentPlayerIndex ;
     let gameOver;
 
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(document.querySelector('#player2').value, 'O'),
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
 
+    return {
+        start,
+    }
 })();
 
 
