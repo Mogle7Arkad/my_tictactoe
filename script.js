3fa5f13cf04332a612c2e85bc01efb71b27b7c7e
Ali George <george7ali@gmail.com>
2023-08-03 19:35:57 +0100

    created check for win and check for tie function to handle task as mention. both implemented on the handleclick

--- script.js
+++ script.js
@@ -80,9 +80,33 @@ const Game = (() => {
 
     return {
         start,
-        handleClick,
+        restart,
+        handleClick
     }
 })();
+
+function checkForWin(board){
+    const winningCombinations =[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for ( let i = 0; i < winningCombinations.length; i++){
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true;
+        }
+    }
+    return false;
+}   
+function checkForTie(board){
+    return board.every(cell => cell !== '')
+}
 
 
 const startButton = document.querySelector('#start-btn');
