9052fcc195389d904a4ac6d007c829ac8cba1555
Ali George <george7ali@gmail.com>
2023-08-03 19:02:35 +0100

    created a player factory to handle player creation

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const Gameboard = (() => {
+    let gameboard = ['', '', '', '', '', '', '', '', '']
+
+    const render = () => {
+        let boardHTML = '';
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class='square' id='square-${index}'>${square}</div>`
+        })
+        document.querySelector('#gameboard').innerHTML = boardHTML;
+
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener('click', Game.handleClick)
+        })
+    }
+
+
+})();
+const createPlayer = (name, mark) => {
+    return{
+        name,
+        mark
+    }
+}
+
+
 const startButton = document.querySelector('#start-btn');
 startButton.addEventListener('click', () => {
     Game.start();
